f82ebac0cb8cf559d27de96c7a6c5f11c74215f1
SSKATE <kms0219kms@naver.com>
2021-12-04 01:33:06 +0000

    d

--- register/notopen.php
+++ register/notopen.php
@@ -9,7 +9,7 @@
             <div class="error-page-content text-center">
               <img class="w-100" src="/static/image/preloader.gif" alt="image">
               <h2 style="font-size: 45px;">사전 예약</h2>
-              <p><div id="stb_form_result"><p>사전 예약이 시작되지 않았습니다. 사전 예약은 2021년 12월 04일 오전으로 예정되어 있습니다.</p></div></p>
+              <p><div id="stb_form_result"><p>사전 예약이 시작되지 않았습니다.<br>사전 예약은 2021년 12월 04일 오전으로 예정되어 있습니다.</p></div></p>
               <div class="back-to-btn">
                 <a href="/"><button class="btn focus-reset">새로고침</button></a>
               </div>
